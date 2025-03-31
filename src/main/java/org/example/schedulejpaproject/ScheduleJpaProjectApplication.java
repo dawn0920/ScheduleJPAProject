2595b279aa3b01fdf1eba53aceb639129aaf3c74
dawn0920 <saoul0920@gmail.com>
2025-03-31 16:54:44 +0900

    Initial commit

---
+++ src/main/java/org/example/schedulejpaproject/ScheduleJpaProjectApplication.java
@@ -0,0 +1,13 @@
+package org.example.schedulejpaproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScheduleJpaProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScheduleJpaProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/example/schedulejpaproject/ScheduleJpaProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.schedulejpaproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScheduleJpaProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
